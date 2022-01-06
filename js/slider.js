33cd8c62efdbc3bf14350864d72fba1d29de811d
Bang1710 <87267619+Bang1710@users.noreply.github.com>
2022-01-06 10:11:43 +0700

    finished on pc

--- js/slider.js
+++ js/slider.js
@@ -1,9 +1,9 @@
 $('.slider').slick({
     infinite: true,
     slidesToShow: 1,
-    // slidesToScroll: 1
-    autoplay: false,
-    autoplaySpeed: 7000,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 6500,
     prevArrow:`<button type='button' 
     class='slick-prev pull-left'>
     <i class="fas fa-chevron-left"></i>
@@ -15,9 +15,9 @@ $('.slider').slick({
     fade: true,
     speed: 900,
     draggable: true,
-    // infinite: true,
-    // cssEase: 'cubic-bezier(0.7, 0, 0.3, 1)',
-    cssEase: 'ease-in-out',
+    infinite: true,
+    cssEase: 'cubic-bezier(0.7, 0, 0.3, 1)',
+    // cssEase: 'ease-in-out',
     touchThreshold: 100
     // centerMode: true,
     // variableWidth: true,
@@ -31,7 +31,7 @@ $('.plan__list').slick({
   dots: true,
   cssEase: 'ease-in-out',
   autoplay:true,
-  autoplaySpeed: 3000,
+  autoplaySpeed: 2500,
 });
 
 $('.topreview__list').slick({
