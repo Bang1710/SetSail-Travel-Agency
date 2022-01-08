189dabff92a2fdecfcc0adc49380822900ed353f
Bang1710 <87267619+Bang1710@users.noreply.github.com>
2022-01-09 01:16:34 +0700

    not finished responsive on tablet - 1:16 - 9/1/2022

--- js/slider.js
+++ js/slider.js
@@ -1,49 +1,82 @@
-$('.slider').slick({
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 6500,
-    prevArrow:`<button type='button' 
+$(".slider").slick({
+	infinite: true,
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	autoplay: true,
+	autoplaySpeed: 6500,
+	prevArrow: `<button type='button' 
     class='slick-prev pull-left'>
     <i class="fas fa-chevron-left"></i>
     </button>`,
-    nextArrow:`<button type='button' 
+	nextArrow: `<button type='button' 
     class='slick-next pull-right'>
     <i class="fas fa-chevron-right"></i>
     </button>`,
-    fade: true,
-    speed: 900,
-    draggable: true,
-    infinite: true,
-    cssEase: 'cubic-bezier(0.7, 0, 0.3, 1)',
-    // cssEase: 'ease-in-out',
-    touchThreshold: 100
-    // centerMode: true,
-    // variableWidth: true,
+	fade: true,
+	speed: 900,
+	draggable: true,
+	infinite: true,
+	cssEase: "cubic-bezier(0.7, 0, 0.3, 1)",
+	touchThreshold: 100,
+	responsive: [
+		{
+			breakpoint: 1025,
+			settings: {
+				arrows: false,
+			},
+		},
+		{
+			breakpoint: 1225,
+			settings: {
+				arrows: true,
+			},
+		},
+	],
+	// cssEase: 'ease-in-out',
+	// centerMode: true,
+	// variableWidth: true,
 });
 
-$('.plan__list').slick({
-  infinite: true,
-  slidesToShow: 4,
-  slidesToScroll: 2,
-  arrows: false,
-  dots: true,
-  cssEase: 'ease-in-out',
-  autoplay:true,
-  autoplaySpeed: 2500,
+$(".plan__list").slick({
+	infinite: true,
+	slidesToShow: 4,
+	slidesToScroll: 2,
+	arrows: false,
+	dots: true,
+	cssEase: "ease-in-out",
+	autoplay: true,
+	autoplaySpeed: 2500,
+	responsive: [
+		{
+			breakpoint: 1025,
+			settings: {
+				slidesToShow: 3,
+			},
+		},
+		{
+			breakpoint: 769,
+			settings: {
+				slidesToShow: 2,
+			},
+		},
+	],
 });
 
-$('.topreview__list').slick({
-  infinite: true,
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  arrows: false,
-  dots: true,
-  cssEase: 'linear',
-  autoplay:true,
-  autoplaySpeed: 2000,
+$(".topreview__list").slick({
+	infinite: true,
+	slidesToShow: 3,
+	slidesToScroll: 1,
+	arrows: false,
+	dots: true,
+	cssEase: "linear",
+	autoplay: false,
+	autoplaySpeed: 2000,
+  responsive: [
+		{
+			breakpoint: 769,
+			settings: {
+				slidesToShow: 2,
+			},
+		},
+	],
 });
-
-
-
